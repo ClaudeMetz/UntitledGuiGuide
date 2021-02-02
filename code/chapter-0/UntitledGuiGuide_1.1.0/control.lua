3b7a33df9db1a8a63f9107ae3e60d772e51b0dfe
Claude Metz <claudearmetz@gmail.com>
2021-02-02 14:05:30 +0100

    Add chapter 0 example code

---
+++ code/chapter-0/UntitledGuiGuide_1.1.0/control.lua
@@ -0,0 +1,9 @@
+-- Make sure the intro cinematic of freeplay doesn't play every time we restart
+-- This is just for convenience, don't worry if you don't understand how this works
+script.on_init(function()
+    local freeplay = remote.interfaces["freeplay"]
+    if freeplay then  -- Disable freeplay popup-message
+        if freeplay["set_skip_intro"] then remote.call("freeplay", "set_skip_intro", true) end
+        if freeplay["set_disable_crashsite"] then remote.call("freeplay", "set_disable_crashsite", true) end
+    end
+end)
